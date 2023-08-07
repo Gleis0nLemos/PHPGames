3ad5b03387f0032a0b30c65fb9a31b91d2482a46
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-07 18:48:53 -0300

    chore: creating guessing game with php

---
+++ guessingGame.php
@@ -0,0 +1,11 @@
+<?php
+
+function play() {
+    echo '<h3>******************************</h3>';
+    echo '<h3>Bem vindo ao jogo de adivinhação!</h3>';
+    echo '<h3>******************************</h3>';
+
+    echo '<h4>Qual o nível de dificuldade você quer?</h4>';
+    echo '<h3>(1)Fácil, (2)Médio, (3)Difícil</h3>';
+    
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once 'guessingGame.php';
+
+play();
