4e3b030d448af77b02f8557c9adb68faafc59cb0
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-07 20:28:11 -0300

    feat: user kick

--- guessingGame.php
+++ guessingGame.php
@@ -8,22 +8,41 @@ function play() {
     echo '<h4>Qual o nível de dificuldade você quer?</h4>';
     echo '<h3>(1)Fácil, (2)Médio, (3)Difícil</h3>';
     
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $secretNumber = rand($min=1, $max=100);
-        $attempts = 0;
-        $points = 1000;
-        
-        $dificuldade = $_POST['dificuldade'];
-        echo "<h4>Você escolheu: $dificuldade</h4>";
+    $secretNumber = 2;#rand($min=1, $max=100);
+    $attempts = 0;
+    $points = 1000;
 
-        if ($dificuldade == 1);
-            echo 'Você está jogando no fácil';
-    } else {
+    if (!isset($_POST['dificuldade'])) {
         echo ' 
         <form action="index.php" method="post">
             <label for="dificuldade">Escolha a dificuldade</label>
             <input type="text" name="dificuldade" id="dificuldade">
             <input type="submit" value="Enviar">
         </form>';
+    } else {
+        $dificuldade = $_POST['dificuldade'];
+        echo "<h4>Você escolheu: $dificuldade</h4>";
+
+        if ($dificuldade == 1) {
+            echo '<h4>Você está jogando no fácil</h4>'; 
+        } elseif ($dificuldade==2) {
+            echo '<h4>Você está jogando no médio</h4>';
+        } elseif ($dificuldade==3) {
+            echo '<h4>Você está jogando no difícil</h4>';
+        } else {
+            echo '<h4>Digite uma dificuldade válida</h4>'; 
+        }
     }
+    
+    if (!isset($_POST['chute'])) {
+        echo ' 
+        <form action="index.php" method="post">
+            <label for="chute">Chute um número de 1 a 100</label>
+            <input type="text" name="chute" id="chute">
+            <input type="submit" value="Enviar">
+        </form>';
+    } else {
+        $chute = $_POST['chute'];
+        echo "<h4>Você chutou: $chute</h4>";
+    }   
 }
