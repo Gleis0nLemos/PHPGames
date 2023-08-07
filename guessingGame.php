ecb907b52977555cc445aa09abc657f4c642fcde
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-07 18:53:35 -0300

    feat: choosing difficulty

--- guessingGame.php
+++ guessingGame.php
@@ -8,4 +8,22 @@ function play() {
     echo '<h4>Qual o nível de dificuldade você quer?</h4>';
     echo '<h3>(1)Fácil, (2)Médio, (3)Difícil</h3>';
     
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $secretNumber = rand($min=1, $max=100);
+        $attempts = 0;
+        $points = 1000;
+        
+        $dificuldade = $_POST['dificuldade'];
+        echo "<h4>Você escolheu: $dificuldade</h4>";
+
+        if ($dificuldade == 1);
+            echo 'Você está jogando no fácil';
+    } else {
+        echo ' 
+        <form action="index.php" method="post">
+            <label for="dificuldade">Escolha a dificuldade</label>
+            <input type="text" name="dificuldade" id="dificuldade">
+            <input type="submit" value="Enviar">
+        </form>';
+    }
 }
